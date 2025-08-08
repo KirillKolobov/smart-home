be33383297bec6cdd8adee8f0b4f05ee6a00db4d
ToropovAlexei <henschel37rus@gmail.com>
2025-08-08 22:36:45 +0300

    [SH-6] - refactoring

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,33 +3,28 @@ use axum::{
     http::StatusCode,
     routing::{get, post},
 };
-use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPoolOptions;
 use std::env;
+
+use crate::config::Config;
+
+mod config;
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
-    dotenv().expect("Failed to load .env file");
-    let port = env::var("PORT").expect("PORT MUST BE DEFINED!");
-    let db_host = env::var("DB_HOST").expect("DB_HOST must be defined in .env");
-    let db_port = env::var("DB_PORT")
-        .expect("DB_PORT must be defined in .env")
-        .parse::<u16>()
-        .expect("DB_PORT must be a valid port number");
-    let db_name = env::var("DB_NAME").expect("DB_NAME must be defined in .env");
-    let db_user = env::var("DB_USER").expect("DB_USER must be defined in .env");
-    let db_pass = env::var("DB_PASSWORD").expect("DB_PASSWORD must be defined in .env");
+    let config = Config::from_env();
     let connection_string = format!(
         "postgres://{user}:{password}@{host}:{port}/{dbname}?sslmode=disable",
-        user = db_user,
-        password = db_pass,
-        host = db_host,
-        port = db_port,
-        dbname = db_name
+        user = config.db_user,
+        password = config.db_pass,
+        host = config.db_host,
+        port = config.db_port,
+        dbname = config.db_name
     );
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&connection_string)
@@ -44,7 +39,7 @@ async fn main() {
         .route("/users", post(create_user));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.port))
         .await
         .expect("Failed to bind TCP listener");
     axum::serve(listener, app).await.unwrap();
