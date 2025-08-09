66ab43a3cb675de34c40b2a77ae783e9b93e77a8
ToropovAlexei <henschel37rus@gmail.com>
2025-08-09 20:01:39 +0300

    [SH-7] - users CRUD validation

--- backend/src/handlers/users.rs
+++ backend/src/handlers/users.rs
@@ -3,15 +3,22 @@ use crate::{
     models::users::{CreateUser, User},
 };
 use axum::{Json, extract::State, http::StatusCode};
+use validator::Validate;
 
 pub async fn create_user(
     State(db): State<Database>,
     Json(payload): Json<CreateUser>,
 ) -> Result<(StatusCode, Json<User>), (StatusCode, String)> {
+    payload
+        .validate()
+        .map_err(|e| (StatusCode::BAD_REQUEST, format!("Validation error: {}", e)))?;
+
     db.create_user(payload)
         .await
         .map(|user| (StatusCode::CREATED, Json(user)))
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+        .map_err(|e| match e {
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+        })
 }
 
 pub async fn get_user(
@@ -21,7 +28,10 @@ pub async fn get_user(
     db.get_user(path.0)
         .await
         .map(|user| (StatusCode::OK, Json(user)))
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => (StatusCode::NOT_FOUND, "User not found".to_string()),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+        })
 }
 
 pub async fn delete_user(
@@ -31,5 +41,8 @@ pub async fn delete_user(
     db.delete_user(path.0)
         .await
         .map(|_| StatusCode::NO_CONTENT)
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => (StatusCode::NOT_FOUND, "User not found".to_string()),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+        })
 }
--- backend/src/models/users.rs
+++ backend/src/models/users.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
+use validator::Validate;
 
 #[derive(Debug, Serialize, FromRow)]
 pub struct User {
@@ -8,9 +9,20 @@ pub struct User {
     pub email: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Validate)]
 pub struct CreateUser {
+    #[validate(length(
+        min = 3,
+        max = 25,
+        message = "Username must be between 3 and 25 characters"
+    ))]
     pub username: String,
+    #[validate(email(message = "Must be a valid email address"))]
     pub email: String,
+    #[validate(length(
+        min = 6,
+        max = 25,
+        message = "Password must be between 6 and 25 characters"
+    ))]
     pub password: String,
 }
